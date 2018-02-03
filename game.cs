be66c241b5146b8a14a1f4674da501193551fd99
CrookedGrandma <k.t.noot@live.nl>
2018-02-03 01:44:57 +0100

    wat dingen of zo

--- game.cs
+++ game.cs
@@ -3,90 +3,121 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.IO;
 using Cloo;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 
 namespace Template {
 
-class Game
-{
-	// when GLInterop is set to true, the fractal is rendered directly to an OpenGL texture
-	bool GLInterop = false;
-	// load the OpenCL program; this creates the OpenCL context
-	static OpenCLProgram ocl = new OpenCLProgram( "../../program.cl" );
-	// find the kernel named 'device_function' in the program
-	OpenCLKernel kernel = new OpenCLKernel( ocl, "device_function" );
-	// create a regular buffer; by default this resides on both the host and the device
-	OpenCLBuffer<int> buffer = new OpenCLBuffer<int>( ocl, 512 * 512 );
-	// create an OpenGL texture to which OpenCL can send data
-	OpenCLImage<int> image = new OpenCLImage<int>( ocl, 512, 512 );
-	public Surface screen;
-	Stopwatch timer = new Stopwatch();
-	float t = 21.5f;
-	public void Init()
-	{
-		// nothing here
-	}
-	public void Tick()
-	{
-		GL.Finish();
-		// clear the screen
-		screen.Clear( 0 );
-		// do opencl stuff
-		if (GLInterop) kernel.SetArgument( 0, image );
-				  else kernel.SetArgument( 0, buffer );
-		kernel.SetArgument( 1, t );
-		t += 0.1f;
- 		// execute kernel
-		long [] workSize = { 512, 512 };
-		long [] localSize = { 32, 4 };
-		if (GLInterop)
-		{
-			// INTEROP PATH:
-			// Use OpenCL to fill an OpenGL texture; this will be used in the
-			// Render method to draw a screen filling quad. This is the fastest
-			// option, but interop may not be available on older systems.
-			// lock the OpenGL texture for use by OpenCL
-			kernel.LockOpenGLObject( image.texBuffer );
-			// execute the kernel
-			kernel.Execute( workSize, localSize );
-			// unlock the OpenGL texture so it can be used for drawing a quad
-			kernel.UnlockOpenGLObject( image.texBuffer );
-		}
-		else
-		{
-			// NO INTEROP PATH:
-			// Use OpenCL to fill a C# pixel array, encapsulated in an
-			// OpenCLBuffer<int> object (buffer). After filling the buffer, it
-			// is copied to the screen surface, so the template code can show
-			// it in the window.
-			// execute the kernel
-			kernel.Execute( workSize, localSize );
-			// get the data from the device to the host
-			buffer.CopyFromDevice();
-			// plot pixels using the data on the host
-			for( int y = 0; y < 512; y++ ) for( int x = 0; x < 512; x++ )
-			{
-				screen.pixels[x + y * screen.width] = buffer[x + y * 512];
-			}
-		}
-	}
-	public void Render() 
-	{
-		// use OpenGL to draw a quad using the texture that was filled by OpenCL
-		if (GLInterop)
-		{
-			GL.LoadIdentity();
-			GL.BindTexture( TextureTarget.Texture2D, image.OpenGLTextureID );
-			GL.Begin( PrimitiveType.Quads );
-			GL.TexCoord2( 0.0f, 1.0f ); GL.Vertex2( -1.0f, -1.0f );
-			GL.TexCoord2( 1.0f, 1.0f ); GL.Vertex2(  1.0f, -1.0f );
-			GL.TexCoord2( 1.0f, 0.0f ); GL.Vertex2(  1.0f,  1.0f );
-			GL.TexCoord2( 0.0f, 0.0f ); GL.Vertex2( -1.0f,  1.0f );
-			GL.End();
-		}
-	}
-}
+    class Game {
+        // when GLInterop is set to true, the fractal is rendered directly to an OpenGL texture
+        bool GLInterop = false;
+        // load the OpenCL program; this creates the OpenCL context
+        static OpenCLProgram ocl = new OpenCLProgram("../../program.cl");
+        // find the kernel named 'device_function' in the program
+        OpenCLKernel kernel = new OpenCLKernel(ocl, "device_function");
+        // create a regular buffer; by default this resides on both the host and the device
+        OpenCLBuffer<uint> pattern = new OpenCLBuffer<uint>(ocl, 512 * 512);
+        OpenCLBuffer<uint> second = new OpenCLBuffer<uint>(ocl, 512 * 512);
+        // create an OpenGL texture to which OpenCL can send data
+        OpenCLImage<int> image = new OpenCLImage<int>(ocl, 512, 512);
+        public Surface screen;
+        Stopwatch timer = new Stopwatch();
+        uint pw, ph;
+
+        // helper function for setting one bit in the pattern buffer
+        void BitSet(uint x, uint y) { pattern[(int)(y * pw + (x >> 5))] |= 1U << (int)(x & 31); }
+        // helper function for getting one bit from the secondary pattern buffer
+        uint GetBit(uint x, uint y) { return (second[(int)(y * pw + (x >> 5))] >> (int)(x & 31)) & 1U; }
+
+        // minimalistic .rle file reader for Golly files (see http://golly.sourceforge.net)
+        public void Init() {
+            StreamReader sr = new StreamReader("../../data/turing_js_r.rle");
+            uint state = 0, n = 0, x = 0, y = 0;
+            while (true) {
+                String line = sr.ReadLine();
+                if (line == null) break; // end of file
+                int pos = 0;
+                if (line[pos] == '#') continue; /* comment line */
+                else if (line[pos] == 'x') { // header
+                    String[] sub = line.Split(new char[] { '=', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    pw = (UInt32.Parse(sub[1]) + 31) / 32;
+                    ph = UInt32.Parse(sub[3]);
+                }
+                else while (pos < line.Length) {
+                        Char c = line[pos++];
+                        if (state == 0) if (c < '0' || c > '9') { state = 1; n = Math.Max(n, 1); } else n = (uint)(n * 10 + (c - '0'));
+                        if (state == 1) // expect other character
+                        {
+                            if (c == '$') { y += n; x = 0; } // newline
+                            else if (c == 'o') for (int i = 0; i < n; i++) BitSet(x++, y); else if (c == 'b') x += n;
+                            state = n = 0;
+                        }
+                    }
+            }
+            // swap buffers
+            for (int i = 0; i < pw * ph; i++) second[i] = pattern[i];
+        }
+        public void Tick() {
+            GL.Finish();
+            // clear the screen
+            screen.Clear(0);
+            // do opencl stuff
+            if (GLInterop) kernel.SetArgument(0, image);
+            else {
+                kernel.SetArgument(0, pattern);
+                kernel.SetArgument(1, second);
+                kernel.SetArgument(2, pw);
+                kernel.SetArgument(3, ph);
+            }
+            // execute kernel
+            long[] workSize = { 512, 512 };
+            long[] localSize = { 32, 4 };
+            if (GLInterop) {
+                // INTEROP PATH:
+                // Use OpenCL to fill an OpenGL texture; this will be used in the
+                // Render method to draw a screen filling quad. This is the fastest
+                // option, but interop may not be available on older systems.
+                // lock the OpenGL texture for use by OpenCL
+                kernel.LockOpenGLObject(image.texBuffer);
+                // execute the kernel
+                kernel.Execute(workSize, localSize);
+                // unlock the OpenGL texture so it can be used for drawing a quad
+                kernel.UnlockOpenGLObject(image.texBuffer);
+            }
+            else {
+                // NO INTEROP PATH:
+                // Use OpenCL to fill a C# pixel array, encapsulated in an
+                // OpenCLBuffer<int> object (buffer). After filling the buffer, it
+                // is copied to the screen surface, so the template code can show
+                // it in the window.
+                // execute the kernel
+                kernel.Execute(workSize, localSize);
+                // get the data from the device to the host
+                pattern.CopyFromDevice();
+                // plot pixels using the data on the host
+                for (int y = 0; y < 512; y++) for (int x = 0; x < 512; x++) {
+                        screen.pixels[x + y * screen.width] = (int)pattern[(x + y * 512)];
+                    }
+
+                // swap buffers
+                for (int i = 0; i < pw * ph; i++) second[i] = pattern[i];
+            }
+        }
+        public void Render() {
+            // use OpenGL to draw a quad using the texture that was filled by OpenCL
+            if (GLInterop) {
+                GL.LoadIdentity();
+                GL.BindTexture(TextureTarget.Texture2D, image.OpenGLTextureID);
+                GL.Begin(PrimitiveType.Quads);
+                GL.TexCoord2(0.0f, 1.0f); GL.Vertex2(-1.0f, -1.0f);
+                GL.TexCoord2(1.0f, 1.0f); GL.Vertex2(1.0f, -1.0f);
+                GL.TexCoord2(1.0f, 0.0f); GL.Vertex2(1.0f, 1.0f);
+                GL.TexCoord2(0.0f, 0.0f); GL.Vertex2(-1.0f, 1.0f);
+                GL.End();
+            }
+        }
+    }
 
 } // namespace Template
--- program.cl
+++ program.cl
@@ -1,46 +1,40 @@
+uint GetBit(__global uint* second, uint pw, uint x, uint y) {
+	uint i = pw * y + (x >> 5);
+    return (second[i] >> (int)(x & 31)) & 1U;
+}
+
+void BitSet(__global uint* pattern, uint pw, uint x, uint y) {
+	uint i = pw * y + (x >> 5);
+	atomic_or(&pattern[i], 1U << (int)(x & 31));
+}
+
 // #define GLINTEROP
 
 #ifdef GLINTEROP
-__kernel void device_function( write_only image2d_t a, float t )
+__kernel void device_function( write_only image2d_t pattern, float t )
 #else
-__kernel void device_function( __global int* a, float t )
+__kernel void device_function( __global uint* pattern, __global uint* second, uint pw, uint ph)
 #endif
 {
 	// adapted from inigo quilez - iq/2013
-	int idx = get_global_id( 0 );
-	int idy = get_global_id( 1 );
-	int id = idx + 512 * idy;
+	uint idx = get_global_id( 0 );
+	uint idy = get_global_id( 1 );
+	uint id = idx + 512 * idy;
 	if (id >= (512 * 512)) return;
-	float2 fragCoord = (float2)( (float)idx, (float)idy ), resolution = (float2)( 512, 512 );
-	float3 col = (float3)( 0.f, 0.f, 0.f );
-	for( int m = 0; m < 4; m++ ) for( int n = 0; n < 4; n++ )
-	{
-		float2 p = -resolution + 2.f * (fragCoord + (float2)( .5f * (float)m, .5f * (float)n ));
-		float w = (float)( 2 * m + n ), l = 0.0f;
-		float time = t + .5f * (1.f / 24.f) * w / 4.f;
-		float zoo = .32f + .2f * cos( .07f * time );
-		float coa = cos( .15f * (1.f - zoo) * time );
-		float sia = sin( .15f * (1.f - zoo) * time );
-		zoo = pow( zoo, 8.f );
-		float2 xy = (float2)( p.x * coa - p.y * sia, p.x * sia + p.y * coa );
-		float2 c = (float2)( -.745f, .186f ) + xy * zoo, z = (float2)( 0.f, 0.f );
-		for( int i = 0; i < 256; i++ )
-		{
-			z = (float2)( z.x * z.x - z.y * z.y, 2.f * z.x * z.y ) + c;
-			if (dot( z, z ) > 65536.f) break; else l += 1.f;
-		}
-		float sl = l - log2( log2( dot( z, z ) ) ) + 4.f;
-		float al = smoothstep( -.1f, 0.f, 1.f );
-		l = mix( l, sl, al );
-		col += .5f + .5f * cos( 3.f + l * 0.15f + (float3)( .0f, .6f, 1.f ) );
-	}
+	float3 col = (float3)( 20.f, 0.f, 0.f );
+	
+	// count active neighbors
+	uint n = GetBit( second, pw, idx - 1, idy - 1 ) + GetBit( second, pw, idx, idy - 1 ) + GetBit( second, pw, idx + 1, idy - 1 ) + GetBit( second, pw, idx - 1, idy ) + 
+			 GetBit( second, pw, idx + 1, idy ) + GetBit( second, pw, idx - 1, idy + 1 ) + GetBit( second, pw, idx, idy + 1 ) + GetBit( second, pw, idx + 1, idy + 1 );
+	if ((GetBit( second, pw, idx, idy ) == 1 && n == 2) || n == 3) { BitSet( pattern, pw, idx, idy ); }
+	
 #ifdef GLINTEROP
 	int2 pos = (int2)(idx,idy);
-	write_imagef( a, pos, (float4)(col * (1.0f / 16.0f), 1.0f ) );
+	write_imagef( pattern, pos, (float4)(col * (1.0f / 16.0f), 1.0f ) );
 #else
 	int r = (int)clamp( 16.0f * col.x, 0.f, 255.f );
 	int g = (int)clamp( 16.0f * col.y, 0.f, 255.f );
 	int b = (int)clamp( 16.0f * col.z, 0.f, 255.f );
-	a[id] = (r << 16) + (g << 8) + b;
+	pattern[id] = (r << 16) + (g << 8) + b;
 #endif
 }
